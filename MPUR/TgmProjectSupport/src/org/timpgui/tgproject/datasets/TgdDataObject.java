b3c2ca913817f506cd115100486b82dc760631e7
jsnel (Joris Snellenburg) <j.snellenburg@vu.nl>
2009-04-09 10:39:17 +0000

    Restored TgdDataObject class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timpgui@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ MPUR/TgmProjectSupport/src/org/timpgui/tgproject/datasets/TgdDataObject.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.timpgui.tgproject.datasets;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataNode;
+import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiDataObject;
+import org.openide.loaders.MultiFileLoader;
+import org.openide.nodes.CookieSet;
+import org.openide.nodes.Node;
+import org.openide.nodes.Children;
+import org.openide.util.Lookup;
+import org.openide.text.DataEditorSupport;
+
+public class TgdDataObject extends MultiDataObject {
+    private Tgd tgd;
+
+    public TgdDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
+        super(pf, loader);
+        //CookieSet cookies = getCookieSet();
+        //cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
+          if (!(FileUtil.toFile(this.getPrimaryFile()) == null)) {
+        try {
+            parseDocument();
+        } catch (IOException ex) {
+            System.out.println("ex=" + ex);
+        }
+          }
+    }
+
+    @Override
+    protected Node createNodeDelegate() {
+        return new DataNode(this, Children.LEAF, getLookup());
+    }
+
+    @Override
+    public Lookup getLookup() {
+        return getCookieSet().getLookup();
+    }
+
+    private Tgd getTgd() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+/**
+     *
+     * @throws IOException
+     */
+    private void parseDocument() throws IOException {
+        if (tgd == null) {
+            tgd = getTgd();
+        } else {
+            java.io.InputStream is = getPrimaryEntry().getFile().getInputStream();
+
+            try {
+                JAXBContext jaxbCtx = JAXBContext.newInstance(tgd.getClass().getPackage().getName());
+                Unmarshaller unmarshaller = jaxbCtx.createUnmarshaller();
+                tgd = (Tgd) unmarshaller.unmarshal(is); //NOI18N
+            } catch (javax.xml.bind.JAXBException ex) {
+                // XXXTODO Handle exception
+                java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+            }
+
+        }
+    }
+}

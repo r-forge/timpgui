347e7ea40601e496659f3de474fc55537bc0c24c
jsnel (Joris Snellenburg) <j.snellenburg@vu.nl>
2009-06-26 10:07:11 +0000

    implemented intial analysis
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timpgui@150 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- MPUR/UI Toolbar/src/org/timpgui/ui/toolbar/StartAnalysis.java
+++ MPUR/UI Toolbar/src/org/timpgui/ui/toolbar/StartAnalysis.java
@@ -12,27 +12,42 @@ import nl.vu.nat.tgmfilesupport.TgmDataObject;
 import nl.vu.nat.tgmodels.tgm.Tgm;
 import org.openide.util.Lookup;
 import org.openide.windows.WindowManager;
+import org.timpgui.structures.DatasetTimp;
+import org.timpgui.structures.TimpResultDataset;
+import org.timpgui.tgproject.datasets.DatasetLoaderInterface;
+import org.timpgui.tgproject.datasets.TimpDatasetDataObject;
 import org.timpgui.timpinterface.TimpInterface;
 
 
 public final class StartAnalysis implements ActionListener {
     private TimpInterface service;
+    private Tgm[] models;
+    private DatasetTimp[] datasets;
+    private final int ITERATIONS = 5;
 
     public void actionPerformed(ActionEvent e) {
         // TODO implement action body
         service = Lookup.getDefault().lookup(TimpInterface.class);
         //Result<TgmDataNode> result = WindowManager.getDefault().findTopComponent("SelectedModelsViewTopComponent").getLookup().lookupResult(TgmDataNode.class);
-        Lookup.Template template = new Lookup.Template(TgmDataObject.class);
-        Lookup.Result<TgmDataObject> foos =  WindowManager.getDefault().findTopComponent("SelectedModelsViewTopComponent").getLookup().lookup(template);
-        Collection<? extends TgmDataObject> col = foos.allInstances();
-        for (Iterator it = col.iterator(); it.hasNext();) {
-            TgmDataObject tgdo = (TgmDataObject) it.next();
-            Tgm tgm = tgdo.getTgm();
-            System.out.println(tgm.getDat().getModelName());
-            //service.
+        Lookup.Template datasetsTemplate = new Lookup.Template(TimpDatasetDataObject.class);
+        Lookup.Result<TimpDatasetDataObject> datasetsLkp =  WindowManager.getDefault().findTopComponent("SelectedDatasetsViewTopComponent").getLookup().lookup(datasetsTemplate);
+        Collection<? extends TimpDatasetDataObject> datasetsCol = datasetsLkp.allInstances();
+        TimpDatasetDataObject[] datasetArr = datasetsCol.toArray(new TimpDatasetDataObject[datasetsCol.size()]);
+        for (int i = 0; i < datasetArr.length; i++) {
+            datasets[i] = datasetArr[i].getDatasetTimp();
         }
 
+        Lookup.Template modelsTemplate = new Lookup.Template(TgmDataObject.class);
+        Lookup.Result<TgmDataObject> modelsLkp =  WindowManager.getDefault().findTopComponent("SelectedModelsViewTopComponent").getLookup().lookup(modelsTemplate);
+        Collection<? extends TgmDataObject> modelsCol = modelsLkp.allInstances();
+        TgmDataObject[] modelArr = modelsCol.toArray(new TgmDataObject[modelsCol.size()]);
+        for (int i = 0; i < modelArr.length; i++) {
+            models[i] = modelArr[i].getTgm();
         }
+
+        service.runAnalysis(datasets, models, ITERATIONS);
+        }
+
 
 
 //        root.getLookup().lookup(TgmDataObject.class);

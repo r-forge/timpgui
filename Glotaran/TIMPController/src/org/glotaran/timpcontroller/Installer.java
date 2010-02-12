2442732048e9b9f0c0c48f59371cd8a60c9eae8e
jsnel (Joris Snellenburg) <j.snellenburg@vu.nl>
2010-02-12 20:31:39 +0000

    updated project template, added code to launch Rserve from Java (linux 64 bit test case only)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timpgui@513 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Glotaran/TIMPController/src/org/glotaran/timpcontroller/Installer.java
+++ Glotaran/TIMPController/src/org/glotaran/timpcontroller/Installer.java
@@ -5,6 +5,7 @@
 package org.glotaran.timpcontroller;
 
 import java.io.File;
+import java.io.OutputStream;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import org.netbeans.api.extexecution.ExecutionDescriptor;
@@ -17,34 +18,35 @@ import org.openide.modules.ModuleInstall;
  */
 public class Installer extends ModuleInstall {
 
-//    private String[] theCommandLineArguments = new String[]{"R","--no-save"};
+    private String[] theCommandLineArguments = new String[]{"/usr/lib64/R/bin/Rcmd","Rserve","--no-save"};
 
     @Override
-    public void restored() {
-        // By default, do nothing.
-        // Put your startup code here.
-//        //ExecutionDescriptor descriptor = new ExecutionDescriptor().controllable(true).frontWindow(true).preExecution(new SomeRunnableToCallBeforeStart()).postExecution(new SomeRunnableToCallAfterExit());
-//        ExecutionDescriptor descriptor = new ExecutionDescriptor().controllable(true).frontWindow(true);
-//        ExecutionService exeService = ExecutionService.newService(
-//        new ProcessLaunch(theCommandLineArguments),
-//        descriptor, "My Process");
-//        Future<Integer> exitCode = exeService.run();
-
+    public void restored() {            
+//            // Create a Callable returning the actual Process.
+//            Callable<Process> processCallable = new ProcessLaunch(theCommandLineArguments);
+//            // Create a descriptor for the UI representation of the process.
+//            ExecutionDescriptor descriptor = new ExecutionDescriptor().controllable(true).frontWindow(true);
+//            descriptor.inputVisible(true);
+//            // Create an instance of ExecutionService and run the Callable<Process>
+//            ExecutionService exeService = ExecutionService.newService(processCallable,descriptor, "My Process");
+//            // Run the process and return the exitcode.
+//            Future<Integer> exitCode = exeService.run();
     }
 
-//    private class ProcessLaunch implements Callable<Process> {
-//
-//        private final String[] commandLine;
-//
-//        public ProcessLaunch(String... commandLine) {
-//            this.commandLine = commandLine;
-//        }
-//
-//        public Process call() throws Exception {
-//            ProcessBuilder pb = new ProcessBuilder(commandLine);
-//            pb.directory(new File(System.getProperty("user.home"))); //NOI18N
-//            pb.redirectErrorStream(true);
-//            return pb.start();
-//        }
-//    }
+
+    private class ProcessLaunch implements Callable<Process> {
+
+        private final String[] commandLine;
+
+        public ProcessLaunch(String... commandLine) {
+            this.commandLine = commandLine;
+        }
+
+        public Process call() throws Exception {
+            ProcessBuilder pb = new ProcessBuilder(commandLine);
+            pb.directory(new File(System.getProperty("user.home"))); //NOI18N
+            pb.redirectErrorStream(true);
+            return pb.start();
+        }
+    }
 }

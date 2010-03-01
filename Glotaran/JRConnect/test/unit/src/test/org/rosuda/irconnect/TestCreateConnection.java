e34e0a4fe143568295cce66dc42a8f95cf163864
jsnel (Joris Snellenburg) <j.snellenburg@vu.nl>
2010-03-01 09:26:52 +0000

    added unit tests to test multiple connections
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timpgui@555 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- Glotaran/JRConnect/test/unit/src/test/org/rosuda/irconnect/TestCreateConnection.java
+++ Glotaran/JRConnect/test/unit/src/test/org/rosuda/irconnect/TestCreateConnection.java
@@ -12,8 +12,21 @@ public class TestCreateConnection extends TestCase{
 
     public void testCreateREngineConnection() {
         final IRConnection irConnection = new REngineConnectionFactory().createRConnection(null);
+        final IRConnection irConnection2 = new REngineConnectionFactory().createRConnection(null);
+        final IRConnection irConnection3 = new REngineConnectionFactory().createRConnection(null);
+        final IRConnection irConnection4 = new REngineConnectionFactory().createRConnection(null);
         assertNotNull(irConnection);
+        assertNotNull(irConnection2);
+        assertNotNull(irConnection3);
+        assertNotNull(irConnection4);
+        assertNotNull(irConnection.eval("rnorm(10^7)").toString());
+        assertNotNull(irConnection2.eval("rnorm(10^7)").toString());
+        assertNotNull(irConnection3.eval("rnorm(10^7)").toString());
+        assertNotNull(irConnection4.eval("rnorm(10^7)").toString());        
         irConnection.close();
+        irConnection2.close();
+        irConnection3.close();
+        irConnection4.close();
     }
 
 }
